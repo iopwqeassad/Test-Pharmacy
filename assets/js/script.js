29b70922fbc8529e96a28075e74c19d057a5591c
Mohamed Gomaa <you@example.com>
2024-08-01 02:20:51 +0300

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,39 @@ function hidePreloader() {
 window.addEventListener('load', function() {
     setTimeout(hidePreloader, 1000); 
 });
+
+
+// Product Details Page
+document.querySelectorAll('.thumbnail').forEach(thumbnail => {
+    thumbnail.addEventListener('click', function() {
+      const src = this.src;
+      document.getElementById('modalImage').src = src;
+    });
+});
+  
+
+
+// Prevent Right Click
+        // Prevent right-click context menu
+        document.addEventListener('contextmenu', function(e) {
+          e.preventDefault();
+      });
+
+      // Prevent F12 key and other keyboard shortcuts
+      document.onkeydown = function(e) {
+          if(e.keyCode == 123) {
+              return false;
+          }
+          if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+              return false;
+          }
+          if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+              return false;
+          }
+          if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+              return false;
+          }
+      };
+
+      // Optional: Display a message when right-click or F12 is attempted
+
